b91613a2b9e1e6019c680f430d55e61822e421ef
Michael Greenwald <michael.greenwald90@gmail.com>
2023-07-30 23:43:47 -0700

    convert shuffle function to generic function

--- src/pages/helpers.tsx
+++ src/pages/helpers.tsx
@@ -1,12 +1,17 @@
 
 // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
-export function shuffleArrayCopy(array: any[]) : any[] {
-    let newArray = array.slice();
+export function shuffleArrayCopy<T>(array: T[]) : T[] {
+    const newArray = array.slice();
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        const temp = newArray[i];
-        newArray[i] = newArray[j];
-        newArray[j] = temp;
+        const temp1 = newArray[i];
+        const temp2 = newArray[j];
+        if (temp1 == undefined || temp2 == undefined) {
+            throw new Error('To-be-shuffled array member is undefined');
+        }
+        newArray[i] = temp2;
+        newArray[j] = temp1;
     }
     return newArray;
 }
+
