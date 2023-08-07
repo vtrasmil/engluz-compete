91ff8907146bde9c10dfcc27b73fd40e211028fe
Michael Greenwald <michael.greenwald90@gmail.com>
2023-08-07 16:52:17 -0700

    narrow type

--- src/server/redis/test.ts
+++ src/server/redis/test.ts
@@ -1,12 +1,15 @@
 import getAblyClient from '../ably/client.js'
-import { kv } from "@vercel/kv";
+
 
 export async function ablyTest() {
     const ably = getAblyClient();
+    
     // get the channel to subscribe to
     const channel = ably.channels.get('quickstart');
     await channel.subscribe('greeting', (message) => {
-        console.log('Received a greeting message in realtime: ' + message.data.toString())
+        if (typeof message.data === 'string') {
+            console.log('Received a greeting message in realtime: ' + message.data)
+        }
     });
     await channel.publish('greeting', 'hello!');
 }
