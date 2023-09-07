82806955f212e41e59b1c27aa7b93ba33d1b62fe
Michael Greenwald <michael.greenwald90@gmail.com>
2023-09-07 14:08:35 -0700

    cleanup

--- src/server/ably/client.ts
+++ src/server/ably/client.ts
@@ -3,23 +3,6 @@ import { configureAbly } from "@ably-labs/react-hooks";
 import { Realtime } from "ably";
 import Ably from "ably/promises";
 
-
-
-
-// const realtime1 = new Ably.Realtime({
-//     key: env.ABLY_API_KEY,
-// });
-
-// export const realtime = realtime1;
-
-// realtime.connection.on('connected', () => {
-//     console.log(`Connected to Ably with connection.id ${realtime.connection.id}`);
-// });
-
-// realtime.connection.on('disconnected', () => {
-//     console.log(`Disconnected from Ably with connection.id ${realtime.connection.id}`);
-// });
-
 const MAX_NUM_CONNECTION_RETRIES = 5;
 export let realtime: Ably.Realtime;
 let retries = 0;
@@ -38,15 +21,6 @@ export function getAblyClient() {
         key: env.ABLY_API_KEY,
     });
     
-    // realtime.connection.on('connected', () => {
-    //     console.log(`Connected to Ably with connection.id ${realtime.connection.id}`);
-    // });
-    
-
-    // realtime.connection.on('disconnected', () => {
-    //     console.log(`Disconnected from Ably with connection.id ${realtime.connection.id}`);
-    // });
-
     realtime.connection.on((connectionState) => {
         const time = new Date().toLocaleString();
         let string = `[${time}] Ably connection ${realtime.connection.id} state change: ${connectionState.previous} --> ${connectionState.current}`;
@@ -58,83 +32,3 @@ export function getAblyClient() {
 
     return realtime;
 }
-
-// function checkForConnection() {
-    
-//     if (retries >= 0) {
-//         // try again
-//         setTimeout(checkForConnection, 1000)
-//     } else if (retries > 4) {
-//         // give up
-//         retries = 0;
-//     }
-
-// }
-
-// export function getAblyClient() : Ably.Realtime | undefined {
-//     if (realtime == undefined) {
-//         return createAblyClient();
-//     } 
-//     if (realtime.connection.state !== 'connected') {
-//         // wait before creating new connection
-//         retries += 1;
-//         if (retries >= 0) {
-//             // try again
-//             console.log(`Waiting for Ably to connect: ${retries + '/' + MAX_NUM_CONNECTION_RETRIES} `)
-//             setTimeout(getAblyClient, 1000, retries);
-//         } else if (retries > MAX_NUM_CONNECTION_RETRIES) {
-//             // give up
-//             retries = 0;
-//             return createAblyClient();
-//         }
-//     } else {
-//         return realtime;
-//     }
-// }
-
-    
-
-
-export async function getAblyClientStringified() {
-    // configureAbly({ key: env.ABLY_API_KEY });
-    // console.log('running getAblyClient()');
-    const storage = require('node-persist');
-    await storage.init( /* options ... */ );
-    const value = await storage.getItem('ablyConnection');
-
-    if (value == undefined) {
-        const realtime = new Ably.Realtime({
-            key: env.ABLY_API_KEY,
-        });
-
-        realtime.connection.on('connected', async () => {
-            console.log(`Connected to Ably with connection.id ${realtime.connection.id}`);
-        });
-        
-        realtime.connection.on('disconnected', () => {
-            console.log(`Disconnected from Ably with connection.id ${realtime.connection.id}`);
-        });
-        
-        const util = require('util');
-        const string = JSON.stringify(util.inspect(realtime));
-        await storage.setItem('ablyConnection', string);
-        return realtime;
-    };
-    
-    const parsed = JSON.parse(value);
-    console.log(`Retrieved ablyConnection: ${parsed}`);
-    return parsed as Ably.Realtime;
-}
-
-// export async function ablyTest() {
-//     const ably = getAblyClient();
-    
-//     // get the channel to subscribe to
-//     const channel = ably.channels.get('quickstart');
-//     await channel.subscribe('greeting', (message) => {
-//         if (typeof message.data === 'string') {
-//             console.log('Received a greeting message in realtime: ' + message.data)
-//         }
-//     });
-//     await channel.publish('greeting', 'hello!');
-// }
