8e6ed6f3b54211bd5db1b74b01dce88711a41c53
Michael Greenwald <michael.greenwald90@gmail.com>
2024-02-04 15:50:07 -0800

    creating a list of bad words

--- src/components/Constants.tsx
+++ src/components/Constants.tsx
@@ -3,3 +3,9 @@ export const MIN_WORD_LENGTH = 2;
 export const ROLL_CHANGE_COLOR = 'lightgreen';
 export const CELL_CHANGE_COLOR = 'gold';
 export const SELECTED_COLOR = 'lightskyblue';
+export const BAD_FOUR_LETTER_WORDS = ['ANUS', 'ARSE', 'CLIT', 'COCK', 'COON',
+    'CUNT', 'DAGO', 'DAMN', 'DICK', 'DIKE', 'DYKE', 'FUCK', 'GOOK', 'HEEB', 'HELL',
+    'HOMO', 'JIZZ', 'KIKE', 'KUNT', 'KYKE', 'MICK', 'MUFF', 'NAZI', 'PAKI', 'PISS',
+    'POON', 'PUTO', 'SHIT', 'SHIZ', 'SLUT', 'SMEG', 'SPIC', 'TARD', 'TITS', 'TWAT',
+    'TWIT', 'WANK'
+];
