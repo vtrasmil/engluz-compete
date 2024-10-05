22b064e8747733fff04eb9e18513abe41dbd450f
Michael Greenwald <michael.greenwald90@gmail.com>
2024-10-05 12:52:30 -0700

    removing MyDragPreview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { CssBaseline } from "@mui/material";
 import { type AppType } from "next/app";
 import { HTML5toTouch } from 'rdndmb-html5-to-touch'; // or any other pipeline
@@ -17,33 +13,6 @@ import {uniqueId} from "~/utils/helpers";
 import { useSessionStorage } from 'usehooks-ts';
 import Layout from '~/components/Layout';
 import AblyRealtimeProvider from "~/components/ably/ably-provider.tsx";
-
-
-
-const MyDragPreview = () => {
-  const preview = usePreview<DraggedLetter, HTMLDivElement>();
-  if (!preview.display) {
-    return null;
-  }
-  const { itemType, item, style, ref, monitor } = preview;
-  const offset = monitor.getClientOffset();
-  if (offset == null) return;
-  const newStyle: CSSProperties = {
-    ...style,
-    width: '50px', height: '50px',
-    WebkitTransform: `translateX(${offset.x - 50}px) translateY(${offset.y - 50}px)`,
-    transform: `translateX(${offset.x - 50}px) translateY(${offset.y - 50}px)`,
-    fontFamily: `"Lexend", sans-serif`,
-    fontWeight: 400,
-    fontSize: `x-large`,
-    zIndex: 20 // letterBlock is 10
-  }
-  return (
-    <div className="item-list__item border border-gray-400" style={newStyle}>
-      <div className={'w-full h-full flex justify-center items-center'}>{item.letters[0]}</div>
-    </div>
-  )
-}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [userId, setUserId] = useSessionStorage('userId', uniqueId('user'))
@@ -64,9 +33,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <DndProvider options={HTML5toTouch}>
           <UserIdProvider userId={userId}>
             <CssBaseline>
-              <MyDragPreview />
               <Layout>
-                <Component {...pageProps} />
+                <Component {...pageProps} style={{fontFamily: `"Lexend", sans-serif`}} />
               </Layout>
             </CssBaseline>
           </UserIdProvider>
