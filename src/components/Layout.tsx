b73d4720cfd5f4ded05ccf1794da2e65def5dcaa
Michael Greenwald <michael.greenwald90@gmail.com>
2024-10-13 22:56:20 -0700

    use Lexend as default font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import { Analytics } from "@vercel/analytics/react"
 import Head from "next/head"
-import { ReactNode } from "react"
+import { type ReactNode } from "react"
 import { cn } from "~/lib/utils"
-import { fontSans } from "~/pages"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import {Lexend} from "next/font/google";
 
 interface LayoutProps {
     children: ReactNode
 }
+
+export const lexend = Lexend({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
 export default function Layout({ children }: LayoutProps) {
     return (
         <>
@@ -15,19 +21,11 @@ export default function Layout({ children }: LayoutProps) {
                 <title>WORDS WORDS WORDS</title>
                 <meta name="description" content="A word game-in-progress" />
                 <link rel="icon" href="/favicon.ico" />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""} />
-                <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap"
-                      rel="stylesheet"/>
                 <meta name="viewport" content="initial-scale=1, width=device-width"/>
             </Head>
             <SpeedInsights/>
             <main
-                className={cn("bg-gray-100 min-h-screen flex items-center justify-center touch-none text-base text-center", fontSans.variable)}>
+                className={cn("bg-gray-100 min-h-screen flex items-center justify-center touch-none text-base text-center", lexend.className)}>
                     {children}
                     <Analytics />
             </main>
